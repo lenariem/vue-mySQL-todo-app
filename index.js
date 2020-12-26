59b08804d169ae7ceca85cf2f495dd6ead05238b
unknown <olena.denysova88@gmail.com>
2020-12-26 17:42:50 +0100

    connected to mySQL

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const sequelize = require('./utils/database')
 const todoRoutes = require('./routes/todo')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -7,13 +8,23 @@ const PORT = process.env.PORT || 3000
 //to use folder public
 app.use(express.static(path.join(__dirname, 'public')))
 
+//routes
+app.use('/api/todo', todoRoutes)
 
 //to use index.html
 app.use((req, res, next) => {
     res.sendFile('/index.html')
 })
 
-//routes
-app.use('/api/todo', todoRoutes)
+//DB connection
+async function start() {
+    try {
+      await sequelize.sync()
+      app.listen(PORT)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  
+  start()
 
-app.listen(PORT)
