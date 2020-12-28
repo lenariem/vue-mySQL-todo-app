e713fc634394ecc2d2b18fc59c6bf366cf62b9b0
unknown <olena.denysova88@gmail.com>
2020-12-28 16:00:53 +0100

    tasks list

--- public/app.js
+++ public/app.js
@@ -8,6 +8,16 @@ new Vue({
         todos: []
       }
     },
+    created() {
+      fetch('/api/todo', {
+        method: 'get'
+      })
+      .then(res => res.json())
+      .then(todos => {
+        this.todos = todos
+      })
+      .catch(e => console.log(e))
+    },
     methods: {
       addTodo() {
         const title = this.todoTitle.trim()
--- routes/todo.js
+++ routes/todo.js
@@ -3,9 +3,10 @@ const Todo = require('../models/todo')
 const router = Router()
 
 //tasks list
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-    
+      const todos = await Todo.findAll()
+      res.status(200).json(todos)
     } catch (err) {
         console.log(err)
         res.status(500).json({
